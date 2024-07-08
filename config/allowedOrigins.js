866f07ffdc1452a45908e916c069f49ba55cb807
hotardaw <hotardaw@gmail.com>
2024-07-08 13:00:16 -0500

    Ready to deploy backend

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,6 @@
 const allowedOrigins = [
   'http://localhost:3500',
-  'https://www.fitsync.io',
-  'https://fitsync.io'
+  'https://www.fitsync.onrender.com',
 ]
 
 module.exports = allowedOrigins
